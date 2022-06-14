2dba8173ab113150e3c199b37687b442b236bb2a
Ogunsolu olawale <63315246+ogunsolu007@users.noreply.github.com>
2022-06-14 04:09:17 -0700

    django

--- Electrified007/Electrified007/urls.py
+++ Electrified007/Electrified007/urls.py
@@ -1,4 +1,4 @@
-"""Electrified007 URL Configuration
+"""electrified007 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
